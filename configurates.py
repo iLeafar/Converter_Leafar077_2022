acb736eaaf27a4e9b996a5dcaa7e5dcbc1abba87
iLeafar <99257111+iLeafar@users.noreply.github.com>
2022-03-23 23:52:39 +0300

    Add files via upload

---
+++ configurates.py
@@ -0,0 +1,7 @@
+TOKEN = "5053085735:AAHk-cpd5h7sLTnrdJDoH4eeYaAVYmOBrGI"
+
+exchanges = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
---
+++ isklycheniya.py
@@ -0,0 +1,36 @@
+import json
+import requests
+from configurates import exchanges
+
+
+class APIException(Exception):
+    pass
+
+
+class Convertor:
+    @staticmethod
+    def get_price(base, sym, amount):
+        try:
+            base_key = exchanges[base.lower()]
+        except KeyError:
+            raise APIException(f"Такая валюта {base} не найдена!")
+
+        try:
+            sym_key = exchanges[sym.lower()]
+        except KeyError:
+            raise APIException(f"Такая валюта {sym} не найдена!")
+
+        if base_key == sym_key:
+            raise APIException(f'Какой смысл переводить {base} в {base}?!')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество {amount}!')
+
+        r = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={base_key}&tsyms={sym_key}")
+        resp = json.loads(r.content)
+        new_price = resp[sym_key] * amount
+        new_price = round(new_price, 3)
+        message = f"Цена {amount} {base} в {sym} : {new_price}"
+        return message
